ba00b6298a5a9534d07a562939c3738cc4d28d50
洪英鑫 <1104350235@qq.com>
2025-06-23 15:14:08 +0800

    feat: 集成 Tailwind CSS 基础配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  
+  // 模块
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  
+  // CSS
+  css: [
+    '~/assets/css/tailwind.css'
+  ],
+  
   devtools: { enabled: true }
 })
