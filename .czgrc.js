5e8dea330ea0d4bb469a8a25aabbd54d44e6210f
洪英鑫 <1104350235@qq.com>
2025-07-09 17:39:29 +0800

    feat: 开启图标

--- .czgrc.js
+++ .czgrc.js
@@ -18,46 +18,59 @@ module.exports = {
       confirmCommit: '是否提交或修改commit ?',
     },
     types: [
-      { value: 'feat', name: 'feat:     ✨  新增功能 | A new feature' },
-      { value: 'fix', name: 'fix:      🐛  修复缺陷 | A bug fix' },
+      {
+        value: 'feat',
+        name: 'feat:     ✨  新增功能 | A new feature',
+        emoji: '✨',
+      },
+      { value: 'fix', name: 'fix:      🐛  修复缺陷 | A bug fix', emoji: '🐛' },
       {
         value: 'docs',
         name: 'docs:     📝  文档更新 | Documentation only changes',
+        emoji: '📝',
       },
       {
         value: 'style',
         name: 'style:    💄  代码格式 | Changes that do not affect the meaning of the code',
+        emoji: '💄',
       },
       {
         value: 'refactor',
         name: 'refactor: ♻️   代码重构 | A code change that neither fixes a bug nor adds a feature',
+        emoji: '♻️',
       },
       {
         value: 'perf',
         name: 'perf:     ⚡️  性能提升 | A code change that improves performance',
+        emoji: '⚡️',
       },
       {
         value: 'test',
         name: 'test:     ✅  测试相关 | Adding missing tests or correcting existing tests',
+        emoji: '✅',
       },
       {
         value: 'build',
         name: 'build:    📦️  构建相关 | Changes that affect the build system or external dependencies',
+        emoji: '📦️',
       },
       {
         value: 'ci',
         name: 'ci:       🎡  持续集成 | Changes to our CI configuration files and scripts',
+        emoji: '🎡',
       },
       {
         value: 'chore',
         name: "chore:    🔨  其他修改 | Other changes that don't modify src or test files",
+        emoji: '🔨',
       },
       {
         value: 'revert',
         name: 'revert:   ⏪️  回退代码 | Reverts a previous commit',
+        emoji: '⏪️',
       },
     ],
-    useEmoji: false,
+    useEmoji: true,
     emojiAlign: 'center',
     useAI: false,
     aiNumber: 3,
